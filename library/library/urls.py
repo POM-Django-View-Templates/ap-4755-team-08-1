7990aef2325c0363d5e7a5a7a2cb0afbf3daaea0
Dmytro Novykov <dimacpoint@gmail.com>
2026-06-08 19:29:52 +0300

    added orders: showing orders to librarian/showing orders to user/ creating order for user/ delete order(should've been closing(working on it) for librarian; added books: showing all books to librarian/user/ single book page; added orders into dashboard

--- library/library/urls.py
+++ library/library/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
-    path('authors/', include('author.urls'))
+    path('authors/', include('author.urls')),
+    path('books/', include('book.urls')),
+    path('orders/', include('order.urls'))
 ]
